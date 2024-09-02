dae5b63bcd1751d1606658f618c027149fd9fe0c
swaraj nath <swaraj.nath.792@gmail.com>
2024-09-02 21:56:12 +0530

    config file updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  mode:"production",
   entry: './src/LoginPage.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,14 +11,21 @@ module.exports = {
     libraryTarget: 'umd',
     globalObject: 'this'
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals(),
+    {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       }
     ]
